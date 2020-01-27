14f06e7433bd0dd58dc56a891ca00827abc1a975
Henri <henri.journel@gmail.com>
2020-01-27 12:18:54 +0100

    Add customer class and Database filling file

--- Banque/Classes/Account.cs
+++ Banque/Classes/Account.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Banque.Classes
 {
-    class Client
+    class Account
     {
     }
 }
---
+++ Banque/Classes/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banque.Classes
+{
+    class Customer
+    {
+        private int id;
+        private String firstName;
+        private String lastName;
+        private String phoneNumber;
+        private static SqlCommand command;
+        private static SqlDataReader reader;
+
+        public int Id { get => id; set => id = value; }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+
+        public void Save()
+        {
+
+        }
+    }
+}
